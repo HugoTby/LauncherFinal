8299273d1145cc0b83ae6f61b4c59cf7ca927821
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2022-10-20 21:38:36 +0200

    3 ème commit 20/10/2022

--- index.php
+++ index.php
@@ -92,18 +92,18 @@
     {
 
 
-    if (($login == "Administrateur" && $password == "wvbt5qry88aijh3kgu22") ||  $login == "Administrateur2"  && $password == "1234")
+    if (($login == "Administrateur" && $password == "wvbt5qry88aijh3kgu22") ||  $login == "tbilhaut"  && $password == "Cavalier King Charles")
     {
-        header("location: acceuil.html");
+        header("location: acceuil.php");
         //Si réussite renvoyer vers -> acceuil.php
     }
 
-    elseif ($login != "Administrateur" || $login != "Administrateur2")
+    elseif ($login != "Administrateur" || $login != "tbilhaut")
     {
         //Si echec renvoyer :
         echo "<p style='color:purple;'>L'Username entré est incorrect.</p>";
     }
-    elseif ($password != "wvbt5qry88aijh3kgu22" || $password != "1234")
+    elseif ($password != "wvbt5qry88aijh3kgu22" || $password != "Cavalier King Charles")
     {
         //Si echec renvoyer :
         echo "<p style='color:purple;'>Le password est incorrect.</p>";
